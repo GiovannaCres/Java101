cef4e4751fe3679db2aa8d766b3f902feb487c31
renat <renat@LAPTOP-H1GTFPMH>
2019-11-16 15:30:43 +0000

    version 1

---
+++ src/javaapplication1/Applicattion1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author renat
+ */
+public class Applicattion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("enter your name");
+        System.out.println("input");
+               
+
+        // TODO code application logic here
+    }
+    
+}
